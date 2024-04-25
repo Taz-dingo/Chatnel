f310778efc674554e6c1e63882b60caf8b86fc0e
Tazdingo <taste_my_blade@outlook.com>
2024-04-25 20:37:46 +0800

    1. next.config.mjs添加images的域名 2. 添加(main)下的layout和servers page，用于显示服务器页面 3. 添加action-tooltip组件，hover时显示，展示一些详细信息 4. 添加navigation-item(服务器列表item)、navigation-action(添加服务器按钮)、navigation-sidebar(侧边栏)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['chatnel.oss-cn-hongkong.aliyuncs.com'], // 替换为你图片的域名
+    },
+};
 
 export default nextConfig;
