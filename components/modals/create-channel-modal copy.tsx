5e1fb2f794ca56edf336d835c84d0c07ec36af53
Tazdingo <taste_my_blade@outlook.com>
2024-05-05 22:41:58 +0800

    1. 添加/api/channels接口，接收POST请求，处理创建频道业务 2. 添加create-channel-modal，创建频道弹窗，用于发送创建频道请求

---
+++ components/modals/create-channel-modal copy.tsx
@@ -0,0 +1,188 @@
+"use client";
+
+import qs from "query-string";
+import axios from "axios";
+import * as z from "zod"; // 数据校验库
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { ChannelType } from "@prisma/client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { FileUpload } from "@/components/file-upload";
+import { useParams, useRouter } from "next/navigation";
+import { useModal } from "@/hooks/use-modal-store";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const channelTypeMap = {
+  TEXT: "文字频道",
+  AUDIO: "音频频道",
+  VIDEO: "视频频道",
+};
+
+// 定义表单的类型
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(1, {
+      message: "请输入频道名称",
+    })
+    .refine((name) => name !== "general", {
+      message: "频道名称不能为'general",
+    }),
+  type: z.nativeEnum(ChannelType),
+});
+
+export const CreateChannelModal = () => {
+  const { isOpen, onClose, type } = useModal();
+  const router = useRouter();
+  const params = useParams();
+
+  const isModalOpen = isOpen && type === "createChannel";
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      type: ChannelType.TEXT,
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      const url = qs.stringifyUrl({
+        url: "/api/channels",
+        query: {
+          serverId: params?.serverId,
+        },
+      });
+
+      await axios.post(url, values);
+      // 刷新页面
+      form.reset();
+      router.refresh();
+      onClose();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const handleClose = () => {
+    form.reset();
+    onClose();
+  };
+
+  return (
+    <Dialog
+      open={isModalOpen}
+      onOpenChange={handleClose}
+    >
+      <DialogContent className="mx-auto bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-center text-2xl font-bold">
+            创建频道
+          </DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8"
+          >
+            <div className="space-y-8 px-6">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                      频道名称
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className="bg-zinc-300/50 border-0 focus-visible:ring-0  text-black"
+                        placeholder="请输入频道名称"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="type"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>频道类型</FormLabel>
+                    <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger
+                          className="bg-zinc-300/50 border-0 
+                        focus:ring-0 text-black ring-offset-0
+                        focus:ring-offset-0 outline-none"
+                        >
+                          <SelectValue placeholder="选择频道类型" />
+                        </SelectTrigger>
+                      </FormControl>
+
+                      <SelectContent>
+                        {Object.values(ChannelType).map((type) => (
+                          <SelectItem
+                            key={type}
+                            value={type}
+                            className=""
+                          >
+                            {channelTypeMap[type]}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button
+                variant={"primary"}
+                disabled={isLoading}
+              >
+                创建
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
